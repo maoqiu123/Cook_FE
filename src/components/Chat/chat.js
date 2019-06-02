d4a55413a79d1bfb53c66b18b4347c0282e08e35
maoqiu123 <565827433@qq.com>
2019-06-02 17:03:08 +0800

    公共聊天室雏形

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Component} from 'react'
 import { Card, Input, Button, Form } from 'antd';
 
-const ws = new WebSocket("wss://echo.websocket.org");
+var ws = new WebSocket("ws://127.0.0.1:8686/");
 ws.onopen = function(evt) {
     console.log("Connection open ...");
     ws.send("Hello WebSockets!");
@@ -10,11 +10,23 @@ ws.onopen = function(evt) {
 
 ws.onmessage = function(evt) {
     console.log( "Received Message: " + evt.data);
-    ws.close();
+    let content = document.getElementById("content")
+    let child = document.createElement("p")
+    content.appendChild(child)
+    child.innerText = evt.data
 };
 
 ws.onclose = function(evt) {
     console.log("Connection closed.");
+};
+window.onbeforeunload=function(){
+    try{
+        ws.send('quit');
+        ws.close();
+        ws=null;
+    }catch(ex){
+        console.log(ex)
+    }
 };
 
 class ChatLayout extends Component{
@@ -23,20 +35,26 @@ class ChatLayout extends Component{
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
-                ws.send("123")
+                ws.send(values.comment)
             }
         });
     };
+    // componentWillUpdate(){
+    //     ws.send('quit')
+    //     ws.close()
+    // }
+
 
     render(){
         const { getFieldDecorator } = this.props.form;
         return(
             <div style={{ background: '#ECECEC', padding: '30px' }}>
                 <Card title="毛球的聊天室" bordered={false} style={{ width: 750, margin:"auto" }}>
-                    <div  style={{width:"suto", height:300}}>
-                        *_* 欢迎进入聊天室 *_*
-                        <br />
-                        <p style={{wordWrap: "break-word", wordB0reak: "break-all"}}>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                    <div  style={{width:"suto", height:300}} id="content">
+                        {/*<p>*_* 欢迎进入聊天室 *_*</p>*/}
+                        {/*<p style={{wordWrap: "break-word", wordB0reak: "break-all"}}>aaaa</p>*/}
+                        {/*<p style={{wordWrap: "break-word", wordB0reak: "break-all"}}>aaaa</p>*/}
+                        {/*<p style={{wordWrap: "break-word", wordB0reak: "break-all"}}>aaaa</p>*/}
                     </div>
                     <Form onSubmit={this.handleSubmit} layout="inline">
                         <Form.Item>
